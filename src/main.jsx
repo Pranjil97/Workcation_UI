f10b5fb9291af157b5caed90428d4c2a630af0d1
Pranjil97 <pranjilkumar97@gmail.com>
2023-07-08 00:44:11 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
-import App from '../src/App.jsx';
-import "../css/tailwind.css"
 
-ReactDom.render(<App />, document.getElementById('root'));
+import App from "./App";
+import "./css/tailwind.css"
+
+const app = document.getElementById("root");
+
+const root = createRoot(app);
+
+root.render(<App />);
+
+// ReactDOM.render(<App />, document.getElementById("root"));
