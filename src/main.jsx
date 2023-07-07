33c7cc0cbe5cb51722323fce743b55d444f3cc56
Pranjil97 <pranjilkumar97@gmail.com>
2023-07-07 20:39:24 +0530

    custom tailwind color

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from '../src/App.jsx';
+import "../css/tailwind.css"
+
+ReactDom.render(<App />, document.getElementById('root'));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: false, // or 'media' or 'class'
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#0fa9e6",
+          light: "#3fbaeb",
+          dark: "#0c87b8",
+        }
+      }
+    },
   },
   plugins: [],
 }
